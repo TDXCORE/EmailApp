cc6281962373362aa72df623ddba7ed0e917bca8
Freddy Rincones <freddyrincones@gmail.com>
2025-05-31 21:45:12 +0000

    fix 3

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 const nextConfig = {
   env: {
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000',
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
   },
   eslint: {
     ignoreDuringBuilds: true,
